12a8468563c96a6bbdd077174371a622fcbd4952
Jehan Veracierta <jehanzao@gmail.com>
2023-02-24 13:05:22 -0400

    ss

--- src/components/StickyFooter.jsx
+++ src/components/StickyFooter.jsx
@@ -5,12 +5,13 @@ import  FavoriteIcon from "@material-ui/icons/Favorite"
 export default function StickyFooter() {
     const classes = useStyles()
     return (
+        
         <footer className={classes.footer}>
             <Container maxWidth="sm">
                 <Typography variant="body1">
                     Created with <FavoriteIcon style={{color: "red"}}/> By Tal Company 
                     <span>
-                        <a href="pagina.com" target="_blank" rel='noreferrer'>Yourpage.com</a>
+                        <a href="pagina.com" target="_blank" rel='noreferrer'>Yamikawa</a>
                     </span>
                 </Typography>
             </Container>
